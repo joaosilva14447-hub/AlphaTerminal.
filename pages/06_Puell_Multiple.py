cc794c867c43f5f78fde417b234475724b9f7551
joaosilva14447-hub <joaosilva14447@gmail.com>
2026-03-09 06:28:51 +0000

    Update page title for Puell Multiple Terminal

--- pages/06_Puell_Multiple.py
+++ pages/06_Puell_Multiple.py
@@ -5,6 +5,7 @@ import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
+
 # High-Performance Terminal Config
 st.set_page_config(page_title="07 Puell Multiple Terminal", layout="wide")
 
